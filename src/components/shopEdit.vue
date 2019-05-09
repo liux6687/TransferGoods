c3b943749de64fe5498ffa4e509c246048cb5707
liuxiang <918112159@qq.com>
2019-05-09 08:48:22 +0800

    登录功能

--- src/components/shopEdit.vue
+++ src/components/shopEdit.vue
@@ -12,77 +12,79 @@
 							<span class="num">{{data.sku_binded_num}}/{{data.sku_num}}</span>
 						</div>
 					</div>
-					<div>
-						<table style="width: 100%;">
-							<tr class="table-title" style="width: 100%;">
-								<td class="table-title-td" style="width: 100%;">
-									<div style="width: 250px;">
-										<span>颜色:</span>
-										<span>{{color}}</span>
-									</div>
-									<div style="width: 330px;">
-										<Col span="5">对应货号:</Col>
-										<Col span="19">
+					<div class="color_list" v-for="(item, index) in colorList" :key="index">
+						<div class="tables">
+							<table style="width: 100%;">
+								<tr class="table-title" style="width: 100%;">
+									<td class="table-title-td" style="width: 100%;">
+										<div style="width: 250px;">
+											<span>颜色:</span>
+											<span>{{item[0].color}}</span>
+										</div>
+										<div style="width: 330px;">
+											<Col span="5">对应货号:</Col>
+											<Col span="19">
+												<selectBox></selectBox>
+											</Col>
+										</div>
+										<div style="width: 150px;">
+											<Col span="12">浮动比例:</Col>
+											<Col span="8"><Input></Input></Col>
+											<Col span="2">%</Col>
+										</div>
+										<div style="width: 250px;">
+											<span class="fl">出售价格按:</span>
+											<Dropdown class="fl" trigger="click">
+												 <Button class="select-dropdown" type="info">
+												   百分比
+												   <Icon type="ios-arrow-down"></Icon>
+												 </Button>
+												<DropdownMenu slot="list">
+													<DropdownItem>百分比</DropdownItem>
+													<DropdownItem>固定差额</DropdownItem>
+												</DropdownMenu>
+											</Dropdown>
+											<Input class="fl" style="width: 80px; line-height: 51px;"></Input>
+										</div>
+										<div class="table-btn-box" style="width: 180px; display: block;">
+											<Button style="width: 80px;" type="success">一键解绑</Button>
+											<Button style="width: 80px;" type="success">批量设置</Button>
+										</div>
+									</td>
+								</tr>
+							</table>
+							<table style="width: 100%;">
+								<tr class="table-main" v-for="(itm, idx) in item" :key="idx" style="width: 100%;">
+									<td style="width: 20%;">
+										<Col span="8">
+											<span>尺码: {{itm.size}}</span>
+										</Col>
+										<Col span="12">
+											<span>店铺价: {{itm.price}}</span>
+										</Col>
+									</td>
+									<td style="width: 40%;">
+										<Col span="4">对应尺码:</Col>
+										<Col span="18">
 											<selectBox></selectBox>
 										</Col>
-									</div>
-									<div style="width: 150px;">
-										<Col span="12">浮动比例:</Col>
+										<Col span="2"><Button type="info">取消</Button></Col>
+									</td>
+									<td class="float-ratio" style="width: 10%;">
 										<Col span="8"><Input></Input></Col>
 										<Col span="2">%</Col>
-									</div>
-									<div style="width: 250px;">
-										<span class="fl">出售价格按:</span>
-										<Dropdown class="fl" trigger="click">
-											 <Button class="select-dropdown" type="info">
-											   百分比
-											   <Icon type="ios-arrow-down"></Icon>
-											 </Button>
-											<DropdownMenu slot="list">
-												<DropdownItem>百分比</DropdownItem>
-												<DropdownItem>固定差额</DropdownItem>
-											</DropdownMenu>
-										</Dropdown>
-										<Input class="fl" style="width: 80px; line-height: 51px;"></Input>
-									</div>
-									<div class="table-btn-box" style="width: 180px; display: block;">
-										<Button style="width: 80px;" type="success">一键解绑</Button>
-										<Button style="width: 80px;" type="success">批量设置</Button>
-									</div>
-								</td>
-							</tr>
-						</table>
+									</td>
+									<td class="sell-pirce" style="width: 20%;">
+										<Col span="8"><Input></Input></Col>
+										<Col span="4">%</Col>
+									</td>
+									<td style="width: 5%;">
+										<Button type="info">解绑</Button>
+									</td>
+								</tr>
+							</table>
+						</div>
 					</div>
-					<table style="width: 100%;">
-						<tr class="table-main" v-for="(item, index) in listData" :key="index" style="width: 100%;">
-							<td style="width: 20%;">
-								<Col span="8">
-									<span>尺码: {{item.size}}</span>
-								</Col>
-								<Col span="12">
-									<span>店铺价: {{item.price}}</span>
-								</Col>
-							</td>
-							<td style="width: 40%;">
-								<Col span="4">对应尺码:</Col>
-								<Col span="18">
-									<selectBox></selectBox>
-								</Col>
-								<Col span="2"><Button type="info">取消</Button></Col>
-							</td>
-							<td class="float-ratio" style="width: 10%;">
-								<Col span="8"><Input></Input></Col>
-								<Col span="2">%</Col>
-							</td>
-							<td class="sell-pirce" style="width: 20%;">
-								<Col span="8"><Input></Input></Col>
-								<Col span="4">%</Col>
-							</td>
-							<td style="width: 5%;">
-								<Button type="info">解绑</Button>
-							</td>
-						</tr>
-					</table>
 				</div>
 				<div class="card-footer">
 					<div class="btn-box">
@@ -102,7 +104,8 @@
 		data() {
 			return {
 				name: "",
-				
+				colorList: [],
+				sku_id_list: [],
 				data: '',
 				listData: [],
 				color: "",
@@ -135,17 +138,41 @@
 			this.$http.get("/api/goods-binding-edit/" + 4).then((res)=> {
 				this.$data.name = res.data.data.data.name;
 				let skus = res.data.data.data.skus;
-				console.log(skus)
-				let result = skus.reduce(function(initArray, item){
-					console.log(initArray)
-					let index = item.color;
-					if(initArray[index]) {
-						initArray[index].push(item)
+				let arr = []
+				let arr_ = []
+				let arr__ = []
+				let sku_id_list = []
+				skus.forEach((item, index) => {
+					arr.push(item.color)
+					sku_id_list.push(item.id)
+				})
+				this.$data.sku_id_list = sku_id_list;
+				arr.forEach((item, index) => {
+					if(arr_.indexOf(item) == -1) {
+						arr_.push(item)
 					}else {
-						initArray[index] = item;
+						arr_[item] = item
 					}
-				},{})
+				})
+				arr_.forEach((item, index) => {
+					item = item.replace(/\s*/g,"");
+					let arr = [];
+					skus.forEach((itm, idx) => {
+						let color = itm.color.replace(/\s*/g,"");
+						if(item == color) {
+							arr.push(itm)
+						}
+					})
+					arr__.push(arr)
+				})
+				this.$data.colorList = arr__;
+			}).then(() => {
+				console.log(JSON.stringify(this.$data.sku_id_list))
+				this.$http.get("/cr/skus_bind_status").then(res => {
+					console.log(res)
+				}) 
 			})
+
 		}
 	}
 </script>
