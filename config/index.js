e4e15f792c6f71bdccd004de9fc4217b4ddc0bfc
haopengfei <1185101239@qq.com>
2019-05-10 11:58:58 +0800

    商品列表修改

--- config/index.js
+++ config/index.js
@@ -18,6 +18,14 @@ module.exports = {
         pathRewrite: {
           '^/api': ''   //需要rewrite的,
         }
+      },
+      '/apis': {    //将www.exaple.com印射为/apis
+        target: 'https://cwa.tosneaker.com/store',  // 接口域名
+        secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true,  //是否跨域
+        pathRewrite: {
+          '^/apis': ''   //需要rewrite的,
+        }
       }
     },
 
